e42efa376e3b9920ff91c5a53294a79f3d7181de
George_716 <george_716@bitbucket.leemail.me>
2014-12-27 00:40:23 +0200

    Wrote the insertTrends method on the DBManager class. Haven't tested it

--- src/TweetCollector/DBManager.java
+++ src/TweetCollector/DBManager.java
@@ -6,11 +6,15 @@
 package TweetCollector;
 
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.Mongo;
 import com.mongodb.MongoURI;
@@ -50,10 +54,53 @@ public class DBManager {
 		this.trendsCollection = this.db.getCollection("trends");
 	}
 
-	public void insertTrend(String json) {
+	public void insertTrends(String json) {
 		
+		//parse the json with the current trends
 		DBObject ob = (DBObject) JSON.parse(json);
-		this.trendsCollection.insert(ob);
+		
+		//get the date of the current trends json file
+		String dateString=(String)ob.get("as_of");
+		
+		//store the trends in a list
+		BasicDBList trends = (BasicDBList) ob.get("trends");
+		
+		for(Object trend : trends){
+			DBObject t = (DBObject) trend;
+			
+			//get the name of the trend
+			String name=(String) t.get("name");
+			
+			BasicDBObject query = new BasicDBObject();
+			ArrayList<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+			obj.add(new BasicDBObject("name", name));
+			obj.add(new BasicDBObject("off_date", null));
+			query.put("$and", obj);
+			DBCursor cursor = trendsCollection.find(query);
+			
+			//check to see if it's a running hot trend or not
+			if(!cursor.hasNext()){
+				BasicDBObject newTrend = new BasicDBObject("name",name).append("on_date", dateString).append("off_date", null);
+				trendsCollection.insert(newTrend);
+			}
+
+		}
+		
+		//now check if some trends should be considered as not running from now on
+		BasicDBObject query = new BasicDBObject();
+		ArrayList<String> list = new ArrayList<>();
+		for(Object trend : trends){
+			DBObject t = (DBObject) trend;
+			list.add((String) t.get("name"));
+		}
+		ArrayList<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("name", new BasicDBObject("$nin", list)));
+		obj.add(new BasicDBObject("off_date", null));
+		query.put("$and", obj);
+		
+		//for every such document update its off_date to the current date
+		BasicDBObject newDocument = new BasicDBObject().append("$set", new BasicDBObject().append("off_date", dateString));
+		trendsCollection.update(query, newDocument);
 	}
 	
 	
